472f3cec345d6fa386c72bc7eb9c27dd74c263ad
gonzito <gonzito1981@gmail.com>
2018-06-14 21:12:44 +0300

    Open the project from repository directory

--- parseSite.py
+++ parseSite.py
@@ -2,8 +2,7 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import pandas as pd
-
-# dfldfldkf
+import numpy as np
 
 def getHTML(url):
     try:
@@ -18,7 +17,6 @@ def getHTML(url):
 
 
 
-
 url = 'http://planecrashinfo.com/1920/1920.htm'
 list = []
 soup = getHTML(url)
@@ -26,45 +24,26 @@ if soup == None:
     print('Invalid Data')
 else:
     table = soup.find('table')
-    labels = table.find('tr').findAll('td')
+    # labels = table.find('tr').findAll('td')
     rows = table.findAll('tr')
     cells = []
     for row in rows:
         cells = row.findAll('td')
-        list.append(cells)
-
-
- 
-
-
-
-
-
-
-    #     for i in range(len(cells)):
-    #         lst.append(cells[i].text)
-    # print(lst)
+        rlist = [cell.text for cell in cells]
+        list.append(rlist)
+    print(list)
+    df = pd.DataFrame(list)
+    print(df)
 
 
 
 
-    # table_headers = soup.find('table', {'id' : 'giftList'}).find('tr').findAll('th')
-    # for header in table_headers:
-    #     print(header.get_text())
 
 
-    # df.to_csv('plcrash.csv')
 
 
-    # try:
-    #     with open('aircr.csv', 'w', newline = '') as file:
-    #         csv_writer = csv.writer(file)
-    #         for item in lst:
-    #             csv_writer.writerow([item])
-    # except ValueError:
-    #     print('!!!')
-    # finally:
-    #     file.close()
+
+
 
 
 
