cd22d77e94879770cfa144854f83700fbbe1cace
gonzito <gonzito1981@gmail.com>
2018-06-16 13:48:32 +0300

    parse fatalities column into common list

--- parseSite.py
+++ parseSite.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 import numpy as np
 
 def getHTML(url):
@@ -15,7 +16,16 @@ def getHTML(url):
         return None
     return bs
 
+def parseFatalities(str = None):
+    my_list = []
+    b = re.compile(r"(\d+)/(\d+)\((\d+)\)")
+    my_tuple = re.findall(b, str)
+    try:
+        my_list = " ".join(my_tuple[0]).split(' ')
+    except IndexError:
+        pass
 
+    return my_list
 
 url = 'http://planecrashinfo.com/1920/1920.htm'
 list = []
@@ -24,19 +34,20 @@ if soup == None:
     print('Invalid Data')
 else:
     table = soup.find('table')
-    # labels = table.find('tr').findAll('td')
     rows = table.findAll('tr')
     cells = []
     for row in rows:
         cells = row.findAll('td')
         rlist = [cell.text for cell in cells]
-        list.append(rlist)
+        common = rlist[:3] + parseFatalities(rlist[3])
+        list.append(common)
     print(list)
-    df = pd.DataFrame(list)
+    labels = ['Date', 'Location / Operator', 'Aircraft Type / Registration', 'Fatalities', 'Cruew', 'Zero']
+    df = pd.DataFrame(list[1:], columns = labels)
     print(df)
 
 
-
+print(parseFatalities('5/2(0)'))
 
 
 
